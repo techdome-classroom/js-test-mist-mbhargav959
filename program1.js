be84794ed3696f63c7d3359e7a9425efc05e1427
mbhargav959 <160421988+mbhargav959@users.noreply.github.com>
2024-02-22 13:46:20 +0530

    solution program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,24 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+function isValid(s) {
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
 
-module.exports = { isValid };
+    for (let char of s) {
+        if (char in pairs) {
+            stack.push(char);
+        } else {
+            const top = stack.pop();
+            if (pairs[top] !== char) {
+                return false;
+            }
+        }
+    }
 
-
+    return stack.length === 0;
+}
+console.log(isValid("()"));       
+console.log(isValid("()[]{}"));   
+console.log(isValid("(]"));      
