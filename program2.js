d59a147a41d20054f4a4068f871a607847ee46d9
mbhargav959 <160421988+mbhargav959@users.noreply.github.com>
2024-02-22 13:47:06 +0530

    solution program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,23 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
-
-
-module.exports={romanToInt}
+function romanToInt(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (i < s.length - 1 && romanValues[s[i]] < romanValues[s[i + 1]]) {
+            result -= romanValues[s[i]];
+        } else {
+            result += romanValues[s[i]];
+        }
+    }
+    return result;
+}
+console.log(romanToInt("III"));      
+console.log(romanToInt("LVIII"));    
+console.log(romanToInt("MCMXCIV")); 
